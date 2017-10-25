c0ec259b029dd5531bf4c851e01e385246763fd1
Afshin Sadeghi <sadeghi@cs.uni-bonn.de>
2017-10-25 09:56:03 +0200

    applying wordnet

--- src/main/scala/net/sansa_stack/kgml/rdf/MergeGraphs.scala
+++ src/main/scala/net/sansa_stack/kgml/rdf/MergeGraphs.scala
@@ -32,11 +32,11 @@ object MergeGraphs {
 
   def main(args: Array[String]) = {
 
-    val input1 = "src/main/resources/dbpedia.nt"
-    val input2 = "src/main/resources/yago.nt"
+    //val input1 = "src/main/resources/dbpedia.nt"
+    //val input2 = "src/main/resources/yago.nt"
 
-    //val input1 = "src/main/resources/dbpediaOnlyAppleobjects.nt"
-    //val input2 = "src/main/resources/yagoonlyAppleobjects.nt"
+    val input1 = "src/main/resources/dbpediaOnlyAppleobjects.nt"
+    val input2 = "src/main/resources/yagoonlyAppleobjects.nt"
 
     val sparkSession = SparkSession.builder
       .master("local[*]")
@@ -51,20 +51,14 @@ object MergeGraphs {
       .getOrCreate()
 
     val triplesRDD = NTripleReader.load(sparkSession, URI.create(input1))
-
     val triplesRDD2 = NTripleReader.load(sparkSession2, URI.create(input2))
 
+    //union of all triples
     val unifiedTriplesRDD = triplesRDD.union(triplesRDD2)
-
-
+    //merging the middle row:  take unique list of the union of predicates
     val unionRelationIDs = (triplesRDD.getPredicates ++ triplesRDD2.getPredicates)
       .map(line => line.toString()).distinct.zipWithUniqueId
-
-
-    case class Node() extends Ordered[Node] {
-      def compare(that: Node): Int = this.toString compare that.toString
-    }
-    // (unionEntityIDs.getNumPartitions)(Ordering[St])
+    //convert entities and predicates to index of numbers
     val unionEntityIDs = (
       triplesRDD.getSubjects
         ++ triplesRDD.getObjects
@@ -75,12 +69,13 @@ object MergeGraphs {
     //distinct does not work on Node objects and the result of zipWithUniqueId become wrong
     // therefore firstly I convert node to string by .map(line => line.toString())
 
-    //val mappedTriples =
 
-    //this.printGraphInfo(unifiedTriplesRDD, unionEntityIDs, unionRelationIDs)
+    this.printGraphInfo(unifiedTriplesRDD, unionEntityIDs, unionRelationIDs)
 
-    this.createCoordinateMatrixModel(unifiedTriplesRDD, unionEntityIDs, unionRelationIDs)
+    var cm = this.createCoordinateMatrix(unifiedTriplesRDD, unionEntityIDs, unionRelationIDs)
 
+    println("matrix rows:" + cm.numRows() + "\n")
+    println("matrix cols:" + cm.numCols() + "\n")
 
     //find their similarity, subjects and predicates. example> barak obama in different KBs
     // find similarites between URI of the same things in differnet  languages  of dbpeida
@@ -95,7 +90,7 @@ object MergeGraphs {
   }
 
   def printGraphInfo(unifiedTriplesRDD: RDD[graph.Triple], unionEntityIDs: RDD[(String, Long)],
-                     unionRelationIDs: RDD[(String, Long)]) = {
+                     unionRelationIDs: RDD[(String, Long)]): Unit = {
     def numEntities = unionEntityIDs.count()
 
     println("Num union Entities " + numEntities + " \n")
@@ -110,77 +105,87 @@ object MergeGraphs {
 
     println("10 first triples of union RDD  \n")
     unifiedTriplesRDD.take(10).foreach(println(_))
-
   }
 
 
   def create3ColumnModel(unifiedTriplesRDD: RDD[graph.Triple], unionEntityIDs: RDD[(String, Long)],
-                       unionRelationIDs: RDD[(String, Long)]) = {
+                         unionRelationIDs: RDD[(String, Long)]): (RDD[(Node, Long)], RDD[(Node, Long)], RDD[(Node, Long)]) = {
 
-  var subjects = unifiedTriplesRDD.map(line => (line.getSubject.toString, line.getSubject))
-  var subjectsWithId = subjects.join(unionEntityIDs).map(line => line._2)
+    var subjects = unifiedTriplesRDD.map(line => (line.getSubject.toString, line.getSubject))
+    var subjectsWithId = subjects.join(unionEntityIDs).map(line => line._2)
 
-  var predicates = unifiedTriplesRDD.map(line => (line.getPredicate.toString, line.getPredicate))
-    .join(unionRelationIDs).map(line => line._2)
-  var objects = unifiedTriplesRDD.map(line => (line.getObject.toString, line.getObject))
-  var objectsWithId = objects.join(unionEntityIDs).map(line => line._2)
+    var predicates = unifiedTriplesRDD.map(line => (line.getPredicate.toString, line.getPredicate))
+      .join(unionRelationIDs).map(line => line._2)
+    var objects = unifiedTriplesRDD.map(line => (line.getObject.toString, line.getObject))
+    var objectsWithId = objects.join(unionEntityIDs).map(line => line._2)
 
-  println("10 subjects  \n")
-  subjectsWithId.take(10).foreach(println(_))
-  println("10 predicates  \n")
-  predicates.take(10).foreach(println(_))
-  println("10 objects  \n")
-  objectsWithId.take(10).foreach(println(_))
+    println("10 subjects  \n")
+    subjectsWithId.take(10).foreach(println(_))
+    println("10 predicates  \n")
+    predicates.take(10).foreach(println(_))
+    println("10 objects  \n")
+    objectsWithId.take(10).foreach(println(_))
 
-}
+     (subjectsWithId, predicates ,objectsWithId)
+  }
+
+  def getMatrixEntityValue(unionEntityIDs: RDD[(String, Long)], id: Long): String = {
+    //reverse id and entity column
+    //val iDtoEntityURIs =  unionEntityIDs.map(line => (line._2, line._1) )
+    unionEntityIDs.filter(line => line._2 == id).first()._1
+  }
+
+  //put two graphs into an 6 column array , 3 String that are a triple and 3 integer that are array index and id relation
   //make two functions for 1: map urI to id, and 2: map id to URI
   // assuming having this two functions, we make matrix of ids
   def createCoordinateMatrixModel(unifiedTriplesRDD: RDD[graph.Triple], unionEntityIDs: RDD[(String, Long)],
-                       unionRelationIDs: RDD[(String, Long)]) = {
+                                  unionRelationIDs: RDD[(String, Long)]): RDD[(String, String, String, Long, Long, Long)] = {
 
     //unionEntityIDs.take(5).foreach(println(_))
     //unionRelationIDs.take(5).foreach(println(_))
 
-    //reverse id and entity column
-    //val iDtoEntityURIs =  unionEntityIDs.map(line => (line._2, line._1) )
-    //iDtoEntityURIs.take(5).foreach(println(_))
-
-    var unionEntityIDsInString  = unionEntityIDs.map(line => (line._1, line._2) )
+    var unionEntityIDsInString = unionEntityIDs.map(line => (line._1, line._2))
 
     //var triples = unifiedTriplesRDD.zipWithIndex()
     var triples2 = unifiedTriplesRDD.map(line =>
-      (line.getSubject.toString, (line.getSubject.toString,line.getPredicate.toString , line.getObject.toString) ))
+      (line.getSubject.toString, (line.getSubject.toString, line.getPredicate.toString, line.getObject.toString)))
 
     var quadruple = triples2.join(unionEntityIDs).map(line => line._2)
 
-    var quintuple = quadruple.map(line => (line._1._2 , (line._1._1, line._1._2, line._1._3, line._2)))
-      .join(unionRelationIDs).map(line => line._2 )
+    var quintuple = quadruple.map(line => (line._1._2, (line._1._1, line._1._2, line._1._3, line._2)))
+      .join(unionRelationIDs).map(line => line._2)
 
-    var sextuple = quintuple.map(line => (line._1._3 , (line._1._1, line._1._2, line._1._3,  line._1._4, line._2)))
-      .join(unionEntityIDs).map(line => line._2 )
-      .map(line => (line._1._1, line._1._2, line._1._3,  line._1._4, line._1._5, line._2))
+    val sextuple = quintuple.map(line => (line._1._3, (line._1._1, line._1._2, line._1._3, line._1._4, line._2)))
+      .join(unionEntityIDs).map(line => line._2)
+      .map(line => (line._1._1, line._1._2, line._1._3, line._1._4, line._1._5, line._2))
 
-    sextuple.take(10).foreach(println(_))
+    //sextuple.take(100).foreach(println(_))
+    sextuple
+  }
+
+  //make two functions for 1: map urI to id, and 2: map id to URI
+  // assuming having this two functions, we make matrix of ids
+  def createCoordinateMatrix(unifiedTriplesRDD: RDD[graph.Triple], unionEntityIDs: RDD[(String, Long)],
+                             unionRelationIDs: RDD[(String, Long)]): CoordinateMatrix = {
+
+    val sextuple = this.createCoordinateMatrixModel(unifiedTriplesRDD, unionEntityIDs, unionRelationIDs)
+    var entries = sextuple.map(line => MatrixEntry(line._4, line._6, line._5))
+    var numCols, numRows = unionEntityIDs.count()
+
+    new CoordinateMatrix(entries, numRows, numCols)
+  }
+
+  def matchPredicatesByWordNet(unionRelationIDs : RDD[(String, Long)]): RDD[(String, Long ,String, Long)] ={
+    // take last section of uri strings
+    // apply a wordnet measurement on all pairs
+    // replace those that are equal
+
+    unionRelationIDs.map(line => line._2 )
 
 
-    //unionEntityIDs.take(10).foreach(println(_))
-
-    /*
-        val entries: RDD[MatrixEntry] = unifiedTriplesRDD.map(triple =>  triple.getObject.)  // an RDD of matrix entries
-        // Create a CoordinateMatrix from an RDD[MatrixEntry].
-        val mat: CoordinateMatrix = new CoordinateMatrix(entries)
-
-        // Get its size.
-        val m = mat.numRows()
-        val n = mat.numCols()
-
-        // Convert it to an IndexRowMatrix whose rows are sparse vectors.
-        val indexedRowMatrix = mat.toIndexedRowMatrix()
-
-    */
-    //done: put that into a matrix then merge the middle row
-    //done: take unique predicates
-    //done: convert entities and predicates to index of numbers
+  def prunePredicateURI(predicateURI : String) : String = {
+    val predicate = predicateURI.split("/").lastOption.toString.toLowerCase
+    predicate
   }
 
 }
