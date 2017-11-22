6dcd3daa9e09ff462c02003dd58e0c55b23b282d
shmkhaled <shmkhaled@gmail.com>
2017-11-22 12:29:36 +0100

    Get the similarity between predicates in the form (predicate string 1, predicate string 2, similarity value)
    not work for predicates with more than one word

--- src/main/scala/net/sansa_stack/kgml/rdf/SimilarityHandler.scala
+++ src/main/scala/net/sansa_stack/kgml/rdf/SimilarityHandler.scala
@@ -36,34 +36,42 @@ class SimilarityHandler(initialThreshold: Double) {
 
   //
   def getMeanWordNetNounSimilarity(string1: String, string2: String): Double = {
-
-    val string1AsNoun = wn.synset(string1, POS.NOUN, 1)
-    val string2AsNoun = wn.synset(string2, POS.NOUN, 1)
-    val nounPathSimilarity = wn.pathSimilarity(string1AsNoun, string2AsNoun)
-    println("nounPathSimilarity = "+nounPathSimilarity)
-
     var nounMeanSim = 0.0
-    if (nounPathSimilarity.equals(1.0)){
-      println("These two predicates are the same")
-      nounMeanSim = 1.0
-    }
-    else {
-      val nounLchSimilarity = wn.lchSimilarity(string1AsNoun, string2AsNoun)
-      println("nounLchSimilarity = "+nounLchSimilarity)
-      //println((nounLchSimilarity * 100).round / 100.toDouble)
-      val nounWupSimilarity = wn.wupSimilarity(string1AsNoun, string2AsNoun)
-      println("nounWupSimilarity = "+nounWupSimilarity)
-      val nounResSimilarity = wn.resSimilarity(string1AsNoun, string2AsNoun)
-      println("nounResSimilarity = "+nounResSimilarity)
-      val nounJcnSimilarity = wn.jcnSimilarity(string1AsNoun, string2AsNoun)
-      println("nounJcnSimilarity = "+nounJcnSimilarity)
-      val nounLinSimilarity = wn.linSimilarity(string1AsNoun, string2AsNoun)
-      println("nounLinSimilarity = "+nounLinSimilarity)
-      val nounLeskSimilarity = wn.leskSimilarity(string1AsNoun, string2AsNoun)
-      println("nounLeskSimilarity = "+nounLeskSimilarity)
-      nounMeanSim = (nounPathSimilarity + nounLchSimilarity + nounWupSimilarity + nounResSimilarity + nounJcnSimilarity
-        + nounLinSimilarity + nounLeskSimilarity) / 7
-    }
+    try
+      {
+        val string1AsNoun = wn.synset(string1, POS.NOUN, 1)
+        val string2AsNoun = wn.synset(string2, POS.NOUN, 1)
+        val nounPathSimilarity = wn.pathSimilarity(string1AsNoun, string2AsNoun)
+        //println("nounPathSimilarity = "+nounPathSimilarity)
+
+
+        if (nounPathSimilarity.equals(1.0)){
+          //println("These two predicates are the same")
+          nounMeanSim = 1.0
+        }
+        else {
+          val nounLchSimilarity = wn.lchSimilarity(string1AsNoun, string2AsNoun)
+          //println("nounLchSimilarity = "+nounLchSimilarity)
+          //println((nounLchSimilarity * 100).round / 100.toDouble)
+          val nounWupSimilarity = wn.wupSimilarity(string1AsNoun, string2AsNoun)
+          //println("nounWupSimilarity = "+nounWupSimilarity)
+          val nounResSimilarity = wn.resSimilarity(string1AsNoun, string2AsNoun)
+          //println("nounResSimilarity = "+nounResSimilarity)
+          val nounJcnSimilarity = wn.jcnSimilarity(string1AsNoun, string2AsNoun)
+          //println("nounJcnSimilarity = "+nounJcnSimilarity)
+          val nounLinSimilarity = wn.linSimilarity(string1AsNoun, string2AsNoun)
+          //println("nounLinSimilarity = "+nounLinSimilarity)
+          val nounLeskSimilarity = wn.leskSimilarity(string1AsNoun, string2AsNoun)
+          //println("nounLeskSimilarity = "+nounLeskSimilarity)
+          nounMeanSim = (nounPathSimilarity + nounLchSimilarity + nounWupSimilarity + nounResSimilarity + nounJcnSimilarity
+            + nounLinSimilarity + nounLeskSimilarity) / 7
+        }
+
+
+      }
+    catch {case e: Exception => nounMeanSim = 0.0}
+
+
     nounMeanSim
   }
 
