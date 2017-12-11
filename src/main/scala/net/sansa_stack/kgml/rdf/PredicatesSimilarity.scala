c1c1d8764d798c191432f596d328c63e22a0c1c0
shmkhaled <shmkhaled@gmail.com>
2017-12-11 13:45:31 +0100

    Get the similarity between predicates in the form (predicate string 1, predicate string 2, similarity value)
    not work for predicates with more than one word
    
    get paird predicates with similarity >= threshold

--- src/main/scala/net/sansa_stack/kgml/rdf/PredicatesSimilarity.scala
+++ src/main/scala/net/sansa_stack/kgml/rdf/PredicatesSimilarity.scala
@@ -15,7 +15,8 @@ import org.apache.spark.rdd.{PairRDDFunctions, RDD, RDDOperationScope}
 class PredicatesSimilarity(sc : SparkContext) extends Serializable{
 //compare two RDD of string and return a new RDD with the two string and the similarity value RDD[string,string,value]
   def matchPredicatesByWordNet(Predicates1 : RDD[(String)], Predicates2 : RDD[(String)]): Array[(String, String, Double)] = {
-   //println("first 5 predicates in KG1 ")
+   println("Number of predicates in KG1 "+ Predicates1.count())
+    println("Number of predicates in KG2 "+ Predicates2.count())
     //Predicates1.distinct().take(5).foreach(println)
     //println("first predicate "+ Predicates1.take(Predicates1.count().toInt).apply(0))
     //println("first 5 predicates in KG2 ")
@@ -23,7 +24,7 @@ class PredicatesSimilarity(sc : SparkContext) extends Serializable{
     //println("Second predicate "+ Predicates2.first())
     //println("first predicate "+ predicatesWithoutURIs2.take(predicatesWithoutURIs2.count().toInt).apply(1))
 
-    val similarityThreshold = 0.45
+    val similarityThreshold = 0.8
     val similarityHandler = new SimilarityHandler(similarityThreshold)
 
     println("##########")
@@ -36,19 +37,33 @@ class PredicatesSimilarity(sc : SparkContext) extends Serializable{
     println("Number of predicates in second KG " + pairsPredicates2.count()) //81
     pairsPredicates2.take(10).foreach(println(_))*/
 
-    val JoindPredicates = (Predicates1.cartesian(Predicates2))
+    val JoindPredicates: RDD[(String,String)] = (Predicates1.cartesian(Predicates2))
     //JoindPredicates.take(10).foreach(println(_))
     println("Number of paird predicates after join " + JoindPredicates.count()) //25353
 
-    val similarPairsRdd = JoindPredicates.collect().
-      map(x => (x._1, x._2, similarityHandler.getPredicateSimilarity(x._1, x._2)))
+
+
+    //var s = new Array[Double](JoindPredicates.count().toInt)
+//    for( i <- 0 to (JoindPredicates.count()-1).toInt){
+//      s(i.toInt) = similarityHandler.getPredicateSimilarity(JoindPredicates.take(JoindPredicates.count().toInt).apply(i.toInt)._1,
+//        JoindPredicates.take(JoindPredicates.count().toInt).apply(i.toInt)._2)
+//    }
+
+
+   // s.take(10).foreach(println(_))
+
+
+
+
+    val similarPairsRdd = JoindPredicates.collect().map(x => (x._1, x._2, similarityHandler.getPredicateSimilarity(x._1, x._2)))
     //println("Similarity between paird predicates = ")
     //similarPairsRdd.take(10).foreach(println(_))
 
+
   //get predicates with similarity >= 0.5
-    val samePredicates = similarPairsRdd.filter(x => x._3 >= similarityThreshold)
-    println("Predicates with similarity >="+ similarityThreshold + "are: "+ samePredicates.length) //64
-    samePredicates.take(samePredicates.length).foreach(println(_))
+   val samePredicates = similarPairsRdd.filter(x => x._3 >= similarityThreshold)
+    println("Predicates with similarity >= "+ similarityThreshold + " are: "+ samePredicates.length) //64
+//    samePredicates.take(samePredicates.length).foreach(println(_))
 
    samePredicates
   }
