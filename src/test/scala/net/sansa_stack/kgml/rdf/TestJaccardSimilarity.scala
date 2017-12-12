4456638cf2f130c400cc7b6c2ee2381422c2f7d9
Afshin Sadeghi <sadeghi@cs.uni-bonn.de>
2017-12-12 13:32:04 +0100

    add similarity for entities(object) adding jaccard similarity adding combination of wordnet and jaccard similarity  in similarity handler

---
+++ src/test/scala/net/sansa_stack/kgml/rdf/TestJaccardSimilarity.scala
@@ -0,0 +1,33 @@
+/**
+  * Created by afshin on 12.12.17.
+  */
+
+package net.sansa_stack.kgml.rdf
+
+object TestJaccardSimilarity{
+
+  def main(args: Array[String]): Unit = {
+
+
+    val similarityThreshold = 0.6
+    val similarityHandler = new SimilarityHandler(similarityThreshold)
+
+    var longPredicate1 = "isCenterOf"
+    var longPredicate2 = "isWorkingWith"
+    var sim = similarityHandler.jaccardPredicateSimilarityWithWordNet(longPredicate1, longPredicate2)
+    print("similarity of " + longPredicate1 + " and "    + longPredicate2 + " is " + sim + "\n")
+
+    longPredicate1 = "isManagerOf"
+    longPredicate2 = "isDirectorOf"
+    sim = similarityHandler.jaccardPredicateSimilarityWithWordNet(longPredicate1, longPredicate2)
+    print("similarity of " + longPredicate1 + " and "    + longPredicate2 + " is " + sim + "\n")
+
+    longPredicate1 = "car"
+    longPredicate2 = "Auto"
+    sim = similarityHandler.jaccardPredicateSimilarityWithWordNet(longPredicate1, longPredicate2)
+    print("similarity of " + longPredicate1 + " and "    + longPredicate2 + " is " + sim + "\n" )
+
+  }
+
+
+}
