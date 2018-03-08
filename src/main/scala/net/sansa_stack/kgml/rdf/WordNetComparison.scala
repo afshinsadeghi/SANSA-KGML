fe79e27b8fdaeb450d152005333b5a3073c1850c
Afshin Sadeghi <sadeghi@cs.uni-bonn.de>
2018-03-08 09:52:52 +0100

    adding predicate match

--- src/main/scala/net/sansa_stack/kgml/rdf/WordNetComparison.scala
+++ src/main/scala/net/sansa_stack/kgml/rdf/WordNetComparison.scala
@@ -1,4 +1,5 @@
 package net.sansa_stack.kgml.rdf
+import org.apache.spark.sql.DataFrame
 import org.apache.spark.sql.functions._
 
 /**
@@ -18,5 +19,20 @@ class WordNetComparison{
     }
     else S})
 
+  val getLiteralValue = udf((S: String) =>  {if(S.startsWith("\"") ) S.split("\"")(1) else S})
 
+
+  def MatchLiterals(df1: DataFrame, df2: DataFrame): Unit = {
+
+    val dF1 = df1.
+      withColumn("Literal1", getLiteralValue(col("object1")))
+
+    val dF2 = df2.
+      withColumn("Literal2", getLiteralValue(col("object2")))
+
+    dF1.createOrReplaceTempView("triple")
+    dF2.createOrReplaceTempView("triple2")
+    val samePredicateAndObject = dF1.join(dF2, dF1("predicate1") <=> dF2("predicate2")
+      && dF1("literal1") <=> dF2("literal2"))
+  }
 }
