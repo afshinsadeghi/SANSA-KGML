513f38eb3d15339ae0c3d1e1ea063761ee4bcfb3
Afshin Sadeghi <sadeghi@cs.uni-bonn.de>
2018-03-08 10:00:25 +0100

    adding predicate match

--- src/main/scala/net/sansa_stack/kgml/rdf/WordNetComparison.scala
+++ src/main/scala/net/sansa_stack/kgml/rdf/WordNetComparison.scala
@@ -1,11 +1,11 @@
 package net.sansa_stack.kgml.rdf
-import org.apache.spark.sql.DataFrame
+import org.apache.spark.sql.{DataFrame, SparkSession}
 import org.apache.spark.sql.functions._
 
 /**
   * Created by afshin on 07.03.18.
   */
-class WordNetComparison{
+class Matching(sparkSession: SparkSession ){
 
 
   /*
@@ -34,5 +34,13 @@ class WordNetComparison{
     dF2.createOrReplaceTempView("triple2")
     val samePredicateAndObject = dF1.join(dF2, dF1("predicate1") <=> dF2("predicate2")
       && dF1("literal1") <=> dF2("literal2"))
+
+
+    samePredicateAndObject.createOrReplaceTempView("sameTypes")
+    println("ranking of subjects based on common type.(I used common predicate and objects which is more general than common type)")
+    val sqlText2 = "SELECT  subject1, subject2, COUNT(*) FROM sameTypes group by subject1,subject2 ORDER BY COUNT(*) DESC"
+    val typedTriples2 = sparkSession.sql(sqlText2)
+    typedTriples2.show(15, 80)
+
   }
 }
